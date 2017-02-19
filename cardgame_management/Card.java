b3f5eff453fbe0cbcbe0d0a9da2988c8835e1fed
3ll3n <ellengrafton01@gmail.com>
2017-02-19 18:53:20 +0000

    updated createDeck test/method

--- cardgame_management/Card.java
+++ cardgame_management/Card.java
@@ -19,8 +19,26 @@ public class Card{
     return this.value;
   }
 
+  // class method - static(self in Ruby)
+
   public static ArrayList<Card> createDeck(){
-     return new ArrayList<Card>();
+    ArrayList<Card> deck = new ArrayList<Card>();
+
+    deck.add(new Card("ace", 1));
+    deck.add(new Card("two", 2));
+    deck.add(new Card("three", 3));
+    deck.add(new Card("four", 4));
+    deck.add(new Card("five", 5));
+    deck.add(new Card("six", 6));
+    deck.add(new Card("seven", 7));
+    deck.add(new Card("eight", 8));
+    deck.add(new Card("nine", 9));
+    deck.add(new Card("ten", 10));
+    deck.add(new Card("jack", 11));
+    deck.add(new Card("queen", 12));
+    deck.add(new Card("king", 13));
+
+    return deck;
   }
 
   
--- cardgame_management/CardTest.java
+++ cardgame_management/CardTest.java
@@ -28,13 +28,62 @@ public class CardTest{
     ArrayList<Card> deck = Card.createDeck();
     assertEquals(13, deck.size());
 
-    Card card1 = deck.get(0);
-    assertEquals("ace", card1.getName());
-    assertEquals(1, card1.getValue());
+    for(int index = 0; index < deck.size(); index++){
+      Card card = deck.get(index);
+      assertEquals(index+1, card.getValue());
+    }
 
-    Card card2 = deck.get(1);
-    assertEquals("two", card2.getName());
-    assertEquals(2, card2.getValue());
+    // Card card1 = deck.get(0);
+    // assertEquals("ace", card1.getName());
+    // assertEquals(1, card1.getValue());
+
+    // Card card2 = deck.get(1);
+    // assertEquals("two", card2.getName());
+    // assertEquals(2, card2.getValue());
+
+    // Card card3 = deck.get(2);
+    // assertEquals("three", card3.getName());
+    // assertEquals(3, card3.getValue());
+
+    // Card card4 = deck.get(3);
+    // assertEquals("four", card4.getName());
+    // assertEquals(4, card4.getValue());
+
+    // Card card5 = deck.get(4);
+    // assertEquals("five", card5.getName());
+    // assertEquals(5, card5.getValue());
+
+    // Card card6 = deck.get(5);
+    // assertEquals("six", card6.getName());
+    // assertEquals(6, card6.getValue());
+
+    // Card card7 = deck.get(6);
+    // assertEquals("seven", card7.getName());
+    // assertEquals(7, card7.getValue());
+
+    // Card card8 = deck.get(7);
+    // assertEquals("eight", card8.getName());
+    // assertEquals(8, card8.getValue());
+
+    // Card card9 = deck.get(8);
+    // assertEquals("nine", card9.getName());
+    // assertEquals(9, card9.getValue());
+
+    // Card card10 = deck.get(9);
+    // assertEquals("ten", card10.getName());
+    // assertEquals(10, card10.getValue());
+
+    // Card card11 = deck.get(10);
+    // assertEquals("jack", card11.getName());
+    // assertEquals(11, card11.getValue());
+
+    // Card card12 = deck.get(11);
+    // assertEquals("queen", card12.getName());
+    // assertEquals(12, card12.getValue());
+
+    // Card card13 = deck.get(12);
+    // assertEquals("king", card13.getName());
+    // assertEquals(13, card13.getValue());
   }
 
 }
